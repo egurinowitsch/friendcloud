77bbcd68d4bee0661d563f535871b809145eda9c
egurinowitsch <egurinowitsch@yahoo.com>
2013-04-24 01:25:29 -0400

    Created a rails application

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Friendcloud::Application.config.secret_token = '33760ffc297ee077ca94eb56fca83dd8b737c925416fff7b9400bedfa3262fe62a483a90be3347fbe4a96e9e5a586114fbdf8eb75529fcd8bfb98f9ba99dd71b'
